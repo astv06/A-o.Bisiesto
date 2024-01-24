db6fe3e6bb3cfcc977a4e1bc0733acebe4203444
astv06 <156257557+astv06@users.noreply.github.com>
2024-01-24 07:37:14 -0400

    Update AñoBisiesto.py
    El código estaba incompleto y cuenta tanto con la versión solo con if como con while

--- AñoBisiesto.py
+++ AñoBisiesto.py
@@ -1,3 +1,41 @@
-print("Por favor escriba un año cualquiera entre 1900 y 2199")
-AEntrada = int(input())
-print(AEntrada)
+print("¿Quieres ver los años uno a uno o no?")
+TAño = input().lower()
+
+if TAño == "si":
+    print("Por favor escriba un año cualquiera de 1900 a 2199")
+    AEntrada = int(input())
+    Año = 1900
+    CanABis = 0
+    while Año <= AEntrada:
+        if Año%4 == 0:
+            if Año%100 != 0:
+                CanABis = CanABis + 1
+                print(Año)
+            elif Año%400 == 0:
+                    CanABis = CanABis + 1
+                    print(Año)
+        Año = Año + 1
+    print("la cantidad de años bisiestos entre 1900 y " + str(AEntrada))
+    print("es de " + str(CanABis))
+    print("y el año " + str(AEntrada))
+    SerONoSer = "no es bisiesto"
+    if AEntrada%4 == 0:
+        if AEntrada != 0:
+            SerONoSer = "es bisiesto"
+        elif AEntrada%400 == 0:
+                SerONoSer = "es bisiesto"
+else:
+     print("Por favor escriba un año cualquiera de 1900 a 2199")
+     AEntrada = int(input())
+     Año = 1900
+     CanABis = (AEntrada - Año)//4
+     print("entre " + str(Año) + " y " + str(AEntrada))
+     print("hubo " + str(CanABis) + "años bisiestos")
+     print("y " + str(AEntrada))
+     SerONoSer = "no es bisiesto"
+     if AEntrada%4 == 0:
+        if AEntrada%100 != 0:
+             SerONoSer = "es bisiesto"
+        elif AEntrada%400 == 0:
+             SerONoSer = "es bisiesto"
+print("el año " + SerONoSer)
